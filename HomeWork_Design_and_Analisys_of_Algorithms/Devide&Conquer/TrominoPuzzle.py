96b101232ae98e6b1994fddfe22864bbceda7602
nhat nguyen <minhnhatsi4@gmail.com>
2024-04-02 10:48:06 +0700

    update and fix TrominoPuzzle.py

--- HomeWork_Design_and_Analisys_of_Algorithms/Devide&Conquer/TrominoPuzzle.py
+++ HomeWork_Design_and_Analisys_of_Algorithms/Devide&Conquer/TrominoPuzzle.py
@@ -1,44 +1,69 @@
-def tromino(board, size, hole, tile=1):
-    # Base case: if the board size is 2x2, place the tromino
+def Tromino(board, size, hole, tile=1, row=None, col=None):
+    if row is None:
+        row = (0, size - 1)
+    if col is None:
+        col = (0, size - 1)
+
     if size == 2:
-        for i in range(2):
-            for j in range(2):
-                if board[hole[0] // size * 2 + i][hole[1] // size * 2 + j] == 0:
-                    board[hole[0] // size * 2 + i][hole[1] // size * 2 + j] = tile
-        return  1
+        for i in range(row[0], row[1] + 1):
+            for j in range(col[0], col[1] + 1):
+                if board[i][j] == 0:
+                    board[i][j] = tile
+        return tile + 1
+    
+    row_mid = (row[1] + row[0]) // 2
+    col_mid = (col[1] + col[0]) // 2
+    newHole = [(row_mid, col_mid), (row_mid, col_mid + 1), (row_mid + 1, col_mid), (row_mid + 1, col_mid + 1)]
+    
+    hole_locate_in_Quandrant = 1
+    if hole[0] <= row_mid:
+        if hole[1] > col_mid:
+            hole_locate_in_Quandrant = 2
+    else: 
+        if hole[1] <= col_mid:
+            hole_locate_in_Quandrant = 3
+        else: 
+            hole_locate_in_Quandrant = 4
+           
+    sm = [((row[0], row_mid), (col[0], col_mid)),  # gocphantu 1
+          ((row[0], row_mid), (col_mid + 1, col[1])),  # gocphantu 2
+          ((row_mid + 1, row[1]), (col[0], col_mid)),  # gocphantu 3
+          ((row_mid + 1, row[1]), (col_mid + 1, col[1]))]  # gocphantu 4
+    contain = [False] * 4  # check coi góc phần tư da vào
+    contain[hole_locate_in_Quandrant - 1] = True
+    for i, (hole_row, hole_col) in enumerate(newHole):
+        if not contain[i]:
+            board[hole_row][hole_col] = tile 
+    tile = tile +1       
+    
 
-    # Recursive case: divide the board into four quadrants
-    half = size // 2
-    centers = [(half - 1, half - 1), (half - 1, half), (half, half - 1), (half, half)]
-    for i, (dx, dy) in enumerate(centers):
-        if hole[0] < half and hole[1] < half:  
-            new_hole = hole if i == 0 else (dx, dy)
-        elif hole[0] < half and hole[1] >= half:  
-            new_hole = hole if i == 1 else (dx, dy)
-        elif hole[0] >= half and hole[1] < half: 
-            new_hole = hole if i == 2 else (dx, dy)
-        else:  
-            new_hole = hole if i == 3 else (dx, dy)
+    
+    for i, (hole_row, hole_col) in enumerate(newHole):
+        if not contain[i]:
+            contain[i] = True
+            tile = Tromino(board, size // 2, (hole_row, hole_col), tile , sm[i][0], sm[i][1])
+        else:
+            tile = Tromino(board, size // 2, hole, tile , sm[i][0], sm[i][1])
 
-        # Adjust the hole position for the next level
-        if i > 0:
-            new_hole = (new_hole[0] % half, new_hole[1] % half)
+    return tile 
 
-        # Recursively solve for the next quadrant
-        tile = tromino(board, half, new_hole, tile)
+hole = (1, 1)
 
-    # Place the tromino in the center
-    board[centers[0][0]][centers[0][1]] = board[centers[1][0]][centers[1][1]] = \
-        board[centers[2][0]][centers[2][1]] = board[centers[3][0]][centers[3][1]] = tile
+n = 6
+size = 2 ** n
+board = [[0] * size for _ in range(size)]
+board[hole[0]][hole[1]] = -1
+Tromino(board, size, hole)
+def print_matrix(matrix):
+    for row in matrix:
+        for elem in row:
+            print(elem, end=" ")
+        print()
+print_matrix(board)
+# matrix_np = np.array(matrix_data)
+# # Make the matrix symmetric
+# symmetric_matrix = (matrix_np + matrix_np.T) / 2
 
-    return  1
-
-# Example usage
-n = 4  # Size of the board (2^n x 2^n)
-board = [[0 for _ in range(n)] for _ in range(n)]
-hole = (1, 1)  # Position of the missing square
-tromino(board, n, hole)
-
-# Print the board
-for row in board:
-    print(' '.join(str(x) for x in row))
+# # Print out the symmetric matrix
+# for row in symmetric_matrix:
+#     print(' '.join(map(lambda x: f'{x:.0f}'.rjust(3), row)))
