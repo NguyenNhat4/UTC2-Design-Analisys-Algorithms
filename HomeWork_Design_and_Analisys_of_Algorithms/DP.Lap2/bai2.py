21e0b41e42ab3474dda8109bde4aaf6018746089
nhatnguyen <minhnhatzzsi4@gmail.com>
2024-06-02 16:41:11 +0700

    fix bai2.py dplap2

--- HomeWork_Design_and_Analisys_of_Algorithms/DP.Lap2/bai2.py
+++ HomeWork_Design_and_Analisys_of_Algorithms/DP.Lap2/bai2.py
@@ -1,11 +1,11 @@
-def compare_meetings(meeting1, meeting2):
-    return meeting1[1] - meeting2[1]
 
 def max_number_of_meetings(start, end):
     n = len(start)
     dp = [0] * n
     dp[0]  = 1
+ 
     meetings = [(start[i], end[i]) for i in range(len(start))]
+    meetings.sort(key=lambda x: x[1])
     for i in range (0,n):
         for j in range(0,i):
             if meetings[i][0] >= meetings[j][1]:
@@ -14,6 +14,6 @@ def max_number_of_meetings(start, end):
     return dp[n-1]
 
 a = [1, 3, 0, 5, 8, 5]
-b = [2, 4, 6, 7, 9, 9]
+b = [5 ,4,2,7,10,6]
 result = max_number_of_meetings(a,b)
 print(result) 
