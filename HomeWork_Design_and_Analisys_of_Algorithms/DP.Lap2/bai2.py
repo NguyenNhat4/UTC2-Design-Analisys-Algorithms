25ff08b8076a58a0752c608550d161b6b91baeef
nhatnguyen <minhnhatzzsi4@gmail.com>
2024-06-02 16:26:33 +0700

    fix bai2.py DP.LAP2

--- HomeWork_Design_and_Analisys_of_Algorithms/DP.Lap2/bai2.py
+++ HomeWork_Design_and_Analisys_of_Algorithms/DP.Lap2/bai2.py
@@ -1,22 +1,19 @@
 def compare_meetings(meeting1, meeting2):
     return meeting1[1] - meeting2[1]
 
-def sap_xep_cuoc_hop(start, end):
+def max_number_of_meetings(start, end):
+    n = len(start)
+    dp = [0] * n
+    dp[0]  = 1
     meetings = [(start[i], end[i]) for i in range(len(start))]
-    
-    sorted_meetings = sorted(meetings, key=compare_meetings)
-    
-    last_end_time = sorted_meetings[0][1]
-    selected_meetings = [sorted_meetings[0]]
-    
-    for current_start, current_end in sorted_meetings[1:]:
-        if current_start >= last_end_time:
-            selected_meetings.append((current_start, current_end))
-            last_end_time = current_end
-    
-    return selected_meetings
+    for i in range (0,n):
+        for j in range(0,i):
+            if meetings[i][0] >= meetings[j][1]:
+                dp[i] = max(dp[i],dp[j]+1)
+     
+    return dp[n-1]
 
-start_times = [1, 3, 0, 5, 8, 5]
-end_times = [2, 4, 6, 7, 9, 9]
-result = sap_xep_cuoc_hop(start_times, end_times)
+a = [1, 3, 0, 5, 8, 5]
+b = [2, 4, 6, 7, 9, 9]
+result = max_number_of_meetings(a,b)
 print(result) 
