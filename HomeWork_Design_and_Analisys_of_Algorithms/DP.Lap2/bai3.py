495cfc5a5393194de62cd5a58727afa30f899ab2
nhatnguyen <minhnhatzzsi4@gmail.com>
2024-06-10 17:06:12 +0700

    fix bai3 dp lab2

--- HomeWork_Design_and_Analisys_of_Algorithms/DP.Lap2/bai3.py
+++ HomeWork_Design_and_Analisys_of_Algorithms/DP.Lap2/bai3.py
@@ -1,36 +1,33 @@
-def thuê_máy(n, start, end, cost):
-    Lend_Computers = [(start[i], end[i], cost[i]) for i in range(n)]
-    def sort_by_end_time(meeting):
-        return meeting[1]
+start = [1,5,2,6,7]
+end = [5,4,7,2,9]
+profit = [1,2,3,4,5] 
+
+def max_profit_for_lending_machine(n,start,end,profit):
+    lending_list = [(start[i],end[i],profit[i]) for i in range(n)]
+    lending_list.sort(key=lambda x: x[1])
+    dp = [0]*n
+    dp[0] = lending_list[0][2]
+    def last_none_conflicts(i,lending_list):
+        for k in range(i-1,-1,-1):
+            if lending_list[i][0] >= lending_list[k][1]:
+                return k
+        return None
     
-    Lend_Computers.sort(key=sort_by_end_time)
+    for i in range(1,n):
+        inc = lending_list[i][2]
+        l = last_none_conflicts(i,lending_list)
+        if l is not None:
+            inc += dp[l]
+        dp[i] = max(inc, dp[i-1])
 
-    def find_last_non_conflict(Lend_Computers, i):
-        for j in range(i - 1, -1, -1):
-            if Lend_Computers[j][1] <= Lend_Computers[i][0]:
-                return j
-        return None # if can't not find any
 
-    def max_rental_dp(Lend_Computers):
-        dp = [0] * n 
-        dp[0] = Lend_Computers[0][2] # assign first element to first profit in 
+    return dp[n-1]
 
-        for i in range(1, n):
-            incl_profit = Lend_Computers[i][2]
-            l = find_last_non_conflict(Lend_Computers, i)
-            if l is not None:
-                incl_profit += dp[l]
 
-            excl_profit = dp[i - 1]
 
-            dp[i] = max(incl_profit, excl_profit)
+print(max_profit_for_lending_machine(len(start),start,end,profit))
 
-        return dp[n - 1]
 
-    return max_rental_dp(Lend_Computers)
 
-n = 4
-start_times = [1, 3, 0, 5]
-end_times = [2, 4, 6, 7]
-costs = [50, 20, 100, 200]
-print(thuê_máy(n, start_times, end_times, costs))
+
+
